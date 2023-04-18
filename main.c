2f6dfc78778270603dd81d2c37b4db5ae707cde5
bari bgf <bougafa.005@gmail.com>
2023-04-18 19:52:44 +0000

    Some improvements

--- main.c
+++ main.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "include/arrays.h"
 
 int main()
 {
-    sarray arr = new_sarray();
-
-    append_s(&arr, "aa");
-    append_s(&arr, "bb");
-    append_s(&arr, "cc");
-
-    print_sarr(arr);
-
+    printf("Hello, World!");
     return 0;
 }
